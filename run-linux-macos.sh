2c9b347c5543013b6239fe16d4f5aab01a089b0c
msk <15199219+muskit@users.noreply.github.com>
2023-12-23 02:50:15 -0800

    update run-linux-macos.sh

--- run-linux-macos.sh
+++ run-linux-macos.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if ! [ -d ".venv" ]; then
-  echo ".venv does not exist; creating virtual environment..."
-  python3 -m venv .venv
-fi
+# if ! [ -d ".venv" ]; then
+#   echo ".venv does not exist; creating virtual environment..."
+#   python3 -m venv .venv
+# fi
 
-source .venv/bin/activate
-python3 -m pip install -r requirements.txt
+# source .venv/bin/activate
+# python3 -m pip install -r requirements.txt
 
 cd src
 python3 main.py

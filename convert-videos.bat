a0f67b57cb26d54168384d29fa883a0fd66b0217
msk <15199219+muskit@users.noreply.github.com>
2024-01-05 22:36:54 -0800

    update convert-videos.bat

--- convert-videos.bat
+++ convert-videos.bat
@@ -1,5 +1,7 @@
+@echo off
+
 :: Set paths first before running!!!
 set video_path="\app\WindowsNoEditor\Mercury\Content\Movie"
 set export_path=".\data\movies"
 
-for %%i in (%video_path%\*.usm) do ffmpeg -f mpegvideo -i "%%i" "%export_path%\%%~ni.mp4"
+for %%i in (%video_path%\*.usm) do ffmpeg -n -f mpegvideo -i "%%i" "%export_path%\%%~ni.mp4"
